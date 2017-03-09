ad44b4d5c8bc801886df2bc5f6d6714dcc168f38
siman-man <k128585@ie.u-ryukyu.ac.jp>
2017-03-10 06:56:35 +0900

    release

--- lib/debugtrace.rb
+++ lib/debugtrace.rb
@@ -1,5 +1,43 @@
-require "debugtrace/version"
+require 'debugtrace/version'
 
 module Debugtrace
-  # Your code goes here...
+  module BacktraceWithArguments
+    def set_backtrace(errinfo)
+      debugtrace = backtrace_locations.zip(errinfo).map do |bl, msg|
+        key = "#{bl.path}:#{bl.label}"
+        [msg, Debugtrace.stack[key].pop].join(' ')
+      end
+
+      super(debugtrace)
+    end
+  end
+end
+
+class Exception
+  prepend Debugtrace::BacktraceWithArguments
+end
+
+module Debugtrace
+  @stack = Hash.new {|h,k| h[k] = [] }
+  STACK_LIMIT = 100
+
+  def self.stack
+    @stack
+  end
+
+  TracePoint.trace(:call) do |tp|
+    key = "#{tp.path}:#{tp.method_id}"
+
+    arg_names = tp.binding.eval("method(:#{tp.method_id}).parameters.map {|n| n[1] }.compact")
+
+    begin
+      values = arg_names.map {|name| tp.binding.local_variable_get(name) }
+    rescue NameError => ex
+      arg_names = []
+      values = []
+    end
+
+    @stack[key] << { arguments: arg_names.zip(values).to_h }
+    @stack[key].shift if @stack[key].size > STACK_LIMIT
+  end
 end
